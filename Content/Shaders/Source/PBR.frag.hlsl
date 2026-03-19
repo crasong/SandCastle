c6116d36c497100d2338f7d3bd40767ed8e472bf
crasong <crasonghwang@gmail.com>
2026-03-19 23:19:38 +0800

    Fix lighting bug
    pretransforming positions to tangent space per-vertex was causing seam artifacts from discontinuous tangents at UV boundaries. So instead of handling things in the vertex shader, we decode the normals in the fragment shader, where we can sample the normal map and apply the TBN appropriately

--- Content/Shaders/Source/PBR.frag.hlsl
+++ Content/Shaders/Source/PBR.frag.hlsl
@@ -1,75 +1,77 @@
-Texture2D<float4> AlbedoTex    : register(t0, space2);
-Texture2D<float4> NormalTex    : register(t1, space2);
-Texture2D<float4> EmissiveTex  : register(t2, space2);
-Texture2D<float4> MetallicTex  : register(t3, space2);
+Texture2D<float4> AlbedoTex : register(t0, space2);
+Texture2D<float4> NormalTex : register(t1, space2);
+Texture2D<float4> EmissiveTex : register(t2, space2);
+Texture2D<float4> MetallicTex : register(t3, space2);
 Texture2D<float4> RoughnessTex : register(t4, space2);
-Texture2D<float4> AOTex        : register(t5, space2);
-SamplerState      Sampler      : register(s0, space2);
+Texture2D<float4> AOTex : register(t5, space2);
+SamplerState Sampler : register(s0, space2);
 
-// cbuffer Light : register(b0, space3) 
+// cbuffer Light : register(b0, space3)
 // {
 //     float3 u_lightcolor;
 // };
 
-struct Input
-{
-    float4 Position : SV_Position;
-    float3 ViewPos  : POSITION0;
-    float3 FragPos  : POSITION1;
-    float3 LightsPos[9] : POSITION2;
-    float2 UV       : TEXCOORD0;
+struct Input {
+  float3 ViewPos : POSITION0;
+  float3 FragPos : POSITION1;
+  float3 LightsPos[9] : POSITION2;
+  float3 Normal : NORMAL0;
+  float3 Tangent : TANGENT0;
+  float3 Bitangent : TANGENT1;
+  float2 UV : TEXCOORD0;
 };
 
-float3 BlinnPhong(float3 viewPos, float3 fragPos, float3 lightPos, float3 lightColor, float2 uv)
-{
-    const float radius = 5;
-    float distance = length(lightPos - fragPos);
-    float3 resultLight = float3(0,0,0);
+float3 BlinnPhong(float3x3 TBN, float3 viewPos, float3 fragPos, float3 lightPos,
+                  float3 lightColor, float2 uv) {
+  const float radius = 5;
+  float distance = length(lightPos - fragPos);
+  float3 resultLight = float3(0, 0, 0);
 
-    if (distance < radius) {
-        float ambientStrength = 0.1f;
-        float3 ambient = ambientStrength * lightColor;
-        //float3 ambient = AOTex.Sample(Sampler, uv).rgb * lightColor;
+  if (distance < radius) {
+    float ambientStrength = 0.1f;
+    float3 ambient = ambientStrength * lightColor;
+    // float3 ambient = AOTex.Sample(Sampler, uv).rgb * lightColor;
 
-        float3 norm = NormalTex.Sample(Sampler, uv).rgb;
-        //norm = normalize(norm * 2.0f - 1.0f);
-        //norm = normalize(norm);
-        float3 lightDir = normalize(lightPos - fragPos);
+    float3 norm = NormalTex.Sample(Sampler, uv).rgb;
+    norm = normalize(norm * 2.0f - 1.0f);
+    norm = normalize(mul(norm, TBN));
+    float3 lightDir = normalize(lightPos - fragPos);
 
-        float diff = max(dot(norm, lightDir), 0.0);
-        float3 diffuse = diff * lightColor;
+    float diff = max(dot(norm, lightDir), 0.0);
+    float3 diffuse = diff * lightColor;
 
-        float specularStrength = 0.2f;
-        float3 viewDir = normalize(viewPos - fragPos);
-        float3 reflectDir = reflect(-lightDir, norm);
-        float3 halfwayDir = normalize(lightDir + viewDir);
-        float spec = pow(max(dot(norm, halfwayDir), 0.0f), 16.0f);
-        float3 specular = specularStrength * spec * lightColor;
+    float specularStrength = 0.2f;
+    float3 viewDir = normalize(viewPos - fragPos);
+    float3 reflectDir = reflect(-lightDir, norm);
+    float3 halfwayDir = normalize(lightDir + viewDir);
+    float spec = pow(max(dot(norm, halfwayDir), 0.0f), 16.0f);
+    float3 specular = specularStrength * spec * lightColor;
 
-        //float attenuation = 1.0f / (distance * distance);
-        float attenuation = radius / (pow(distance, 2) + 1);
+    // float attenuation = 1.0f / (distance * distance);
+    float attenuation = radius / (pow(distance, 2) + 1);
 
-        diffuse *= attenuation;
-        specular *= attenuation;
-        ambient *= attenuation;
-        //resultLight = diffuse + specular + ambient;
-        //resultLight = diffuse + specular;
-        resultLight = norm;
-    }
+    diffuse *= attenuation;
+    specular *= attenuation;
+    ambient *= attenuation;
+    // resultLight = diffuse + specular + ambient;
+    resultLight = diffuse + specular;
+    // resultLight = norm;
+  }
 
-    return resultLight;
+  return resultLight;
 }
 
-float4 main(Input input) : SV_Target0
-{
-    float3 u_lightcolor = float3(1.0f, 1.0f, 1.0f);
-    float gamma = 1.0/2.2;
-    float4 texColor = AlbedoTex.Sample(Sampler, input.UV);
-    float3 result = texColor.rgb * 0.1f; 
-    for (int i = 0; i < 9; ++i) {
-        result += BlinnPhong(input.ViewPos, input.FragPos, input.LightsPos[i], u_lightcolor, input.UV);
-    }
-    //result *= texColor.rgb;
-    //result = pow(result, float3(gamma, gamma, gamma));
-    return float4(result, 1.0f);
+float4 main(Input input) : SV_Target0 {
+  float3x3 TBN = float3x3(input.Tangent, input.Bitangent, input.Normal);
+  float3 u_lightcolor = float3(1.0f, 1.0f, 1.0f);
+  float gamma = 1.0 / 2.2;
+  float4 texColor = AlbedoTex.Sample(Sampler, input.UV);
+  float3 result = texColor.rgb * 0.1f;
+  for (int i = 0; i < 9; ++i) {
+    result += BlinnPhong(TBN, input.ViewPos, input.FragPos, input.LightsPos[i],
+                         u_lightcolor, input.UV);
+  }
+  result *= texColor.rgb;
+  // result = pow(result, float3(gamma, gamma, gamma));
+  return float4(result, 1.0f);
 }
--- Content/Shaders/Source/PBR.vert.hlsl
+++ Content/Shaders/Source/PBR.vert.hlsl
@@ -1,55 +1,46 @@
 #include "Common.hlsl"
-cbuffer Camera : register(b0, space1)
-{
-    float4x4 u_view;
-    float4x4 u_proj;
-    float4x4 u_viewProj;
-    float3   u_viewPos;
+cbuffer Camera : register(b0, space1) {
+  float4x4 u_view;
+  float4x4 u_proj;
+  float4x4 u_viewProj;
+  float3 u_viewPos;
 };
 
-cbuffer Model : register(b1, space1)
-{
-    float4x4 u_model;
+cbuffer Model : register(b1, space1) { float4x4 u_model; };
+
+cbuffer Lights : register(b2, space1) { Light u_lights[9]; };
+
+struct Input {
+  float3 Position : POSITION0;
+  float3 Normal : NORMAL0;
+  float3 Tangent : TANGENT0;
+  float3 Bitangent : TANGENT1;
+  float2 UV : TEXCOORD0;
 };
 
-cbuffer Lights : register(b2, space1)
-{
-    Light u_lights[9];
+struct Output {
+  float4 Position : SV_Position;
+  float3 ViewPos : POSITION0;
+  float3 FragPos : POSITION1;
+  float3 LightsPos[9] : POSITION2;
+  float3 Normal : NORMAL0;
+  float3 Tangent : TANGENT0;
+  float3 Bitangent : TANGENT1;
+  float2 UV : TEXCOORD0;
 };
 
-struct Input
-{
-    float3 Position : POSITION0;
-    float3 Normal   : NORMAL0;
-    float3 Tangent  : TANGENT0;
-    float3 Bitangent: TANGENT1;
-    float2 UV       : TEXCOORD0;
-};
-
-struct Output
-{
-    float4 Position : SV_Position;
-    float3 ViewPos  : POSITION0;
-    float3 FragPos  : POSITION1;
-    float3 LightsPos[9] : POSITION2;
-    float2 UV       : TEXCOORD0;
-};
-
-Output main(Input input)
-{
-    Output output;
-    
-    float3 Normal    = normalize(mul(u_model, float4(input.Normal,    0.0f))).xyz;
-    float3 Tangent   = normalize(mul(u_model, float4(input.Tangent,   0.0f))).xyz;
-    float3 Bitangent = normalize(mul(u_model, float4(input.Bitangent, 0.0f))).xyz;
-    float3x3 TBN = transpose(float3x3(Tangent, Bitangent, Normal));
-    float4 vertPos  = mul(u_model, float4(input.Position, 1.0f));
-    output.Position = mul(u_viewProj, vertPos);
-    output.ViewPos  = mul(TBN, u_viewPos);
-    output.FragPos  = mul(TBN, vertPos.xyz);
-    for (int i = 0; i < 9; ++i) {
-        output.LightsPos[i] = mul(TBN, u_lights[i].position);
-    }
-    output.UV = input.UV;
-    return output;
+Output main(Input input) {
+  Output output;
+  float4 vertPos = mul(u_model, float4(input.Position, 1.0f));
+  output.Position  = mul(u_viewProj, vertPos);
+  output.ViewPos   = u_viewPos;
+  output.FragPos   = vertPos.xyz;
+  output.Normal    = normalize(mul(u_model, float4(input.Normal,    0.0f))).xyz;
+  output.Tangent   = normalize(mul(u_model, float4(input.Tangent,   0.0f))).xyz;
+  output.Bitangent = normalize(mul(u_model, float4(input.Bitangent, 0.0f))).xyz;
+  for (int i = 0; i < 9; ++i) {
+    output.LightsPos[i] = u_lights[i].position;
+  }
+  output.UV = input.UV;
+  return output;
 }
