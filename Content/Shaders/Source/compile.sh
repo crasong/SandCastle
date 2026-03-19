0604bb1708634f8c12cc787c7ac9839f8c0e343e
crasong <crasonghwang@gmail.com>
2026-03-19 23:15:37 +0800

    Update build preset and shader compilation steps
    Preset was using vscode specific field
    
    assimp comes with it's own unzip.h. so use that instead of relying on installed unzip.h
    
    replace POST_BUILD commands for shader compile and content copy with a dependency-tracked setup. This let's me run cmake --build build even just to recompile shader only changes

--- Content/Shaders/Source/compile.sh
+++ Content/Shaders/Source/compile.sh
@@ -1,9 +1,15 @@
 # Requires shadercross CLI installed from SDL_shadercross
+# DXIL compilation is skipped on non-Windows platforms (crashes on Linux)
+case "$(uname -s)" in
+    MINGW*|MSYS*|CYGWIN*|Windows_NT) COMPILE_DXIL=1 ;;
+    *) COMPILE_DXIL=0 ;;
+esac
+
 for filename in *.vert.hlsl; do
     if [ -f "$filename" ]; then
         shadercross "$filename" -o "../Compiled/SPIRV/${filename/.hlsl/.spv}"
         shadercross "$filename" -o "../Compiled/MSL/${filename/.hlsl/.msl}"
-        shadercross "$filename" -o "../Compiled/DXIL/${filename/.hlsl/.dxil}"
+        [ "$COMPILE_DXIL" -eq 1 ] && shadercross "$filename" -o "../Compiled/DXIL/${filename/.hlsl/.dxil}"
     fi
 done
 
@@ -11,7 +17,7 @@ for filename in *.frag.hlsl; do
     if [ -f "$filename" ]; then
         shadercross "$filename" -o "../Compiled/SPIRV/${filename/.hlsl/.spv}"
         shadercross "$filename" -o "../Compiled/MSL/${filename/.hlsl/.msl}"
-        shadercross "$filename" -o "../Compiled/DXIL/${filename/.hlsl/.dxil}"
+        [ "$COMPILE_DXIL" -eq 1 ] && shadercross "$filename" -o "../Compiled/DXIL/${filename/.hlsl/.dxil}"
     fi
 done
 
@@ -19,7 +25,8 @@ for filename in *.comp.hlsl; do
     if [ -f "$filename" ]; then
         shadercross "$filename" -o "../Compiled/SPIRV/${filename/.hlsl/.spv}"
         shadercross "$filename" -o "../Compiled/MSL/${filename/.hlsl/.msl}"
-        shadercross "$filename" -o "../Compiled/DXIL/${filename/.hlsl/.dxil}"
+        [ "$COMPILE_DXIL" -eq 1 ] && shadercross "$filename" -o "../Compiled/DXIL/${filename/.hlsl/.dxil}" || true
     fi
 done
 #read -p "Press any key to continue" x
+exit 0
